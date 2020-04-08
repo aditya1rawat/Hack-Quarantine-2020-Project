81be7a05cb63f91d41222cc8961efe78fda37b35
JP-Vela <jangofett122003@gmail.com>
2020-04-08 01:39:34 -0700

    Fixed some bugs in amISafe.

--- jsAppFiles/map.js
+++ jsAppFiles/map.js
@@ -83,7 +83,7 @@ var circle = L.circle([0, 0], {
                 c.addTo(mymap);
             }
             //console.log("Radar.io status:"+status);
-            console.log("New ok 9");
+            console.log("New ok 10");
             var message = {'owner_id': client.auth.user.id, 'user_id':user._id,  'lat': location.latitude, 'long':location.longitude};
 
 
